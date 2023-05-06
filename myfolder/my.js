8842d73095210d906b55a521acfe936c751d1746
EledikoDcoder <eledikofaruq@gmail.com>
2023-05-06 21:45:24 +0100

    new folder created

--- myfolder/my.js
+++ myfolder/my.js
@@ -3,7 +3,7 @@
 
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
-    ('window-scroll', window.screenY > 0)
+    ('window-scroll', window.scrollY > 0)
 })
 
 
